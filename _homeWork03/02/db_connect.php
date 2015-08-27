48d718f7c64e3d9130a7529ea475c66e48ffe194
unknown <D:\_MAIL>
2015-08-27 16:34:45 +0300

    HW03 task02

---
+++ _homeWork03/02/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: support
+ * Date: 27.08.2015
+ * Time: 16:34
+ */
---
+++ _homeWork03/02/index.php
@@ -0,0 +1,25 @@
+<?php?>
+
+
+<html>
+<head>
+    <title>Работа с БД</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<form>
+    <p>Выбрать таблицу:</p>
+    <select>
+        <option>Пункт 1</option>
+        <option>Пункт 2</option>
+    </select>
+    <p>Выбрать формат:</p>
+    <select>
+        <option>csv</option>
+        <option>json</option>
+        <option>xml</option>
+    </select>
+    <p><input type="submit" name="download" value="Export"></p>
+</form>
+</body>
+</html>
